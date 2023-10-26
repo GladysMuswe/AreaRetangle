8ed6ab70684056571c1c1f687bbad1f6320660ce
student <student@vlab.local>
2023-10-26 11:58:34 +0200

    Initial commit

---
+++ src/RectangleArea.java
@@ -0,0 +1,30 @@
+public class AreaRectangle {
+    //RectangleArea class
+    class RectangleArea {
+        private double length;
+        private double width;
+
+        public RectangleArea(double length,double width){
+            this.length=length;
+            this.width=width;
+
+        }
+
+    }  public  double calculateArea(){
+        return length*width;
+    }
+}
+// Main class
+public class Main{
+    public static void main(String[]args) {
+        RectangleArea(5.0, 3.0);
+        double area = rectangle.calculateArea();
+        system.out.print("The area of the rectangle is:" + area);
+
+
+
+
+    }
+
+    }
+
